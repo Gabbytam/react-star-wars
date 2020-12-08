0876d2c12a35cd0542169dfff9929abaa0f3c0b5
Gabbytam <gtam2@fordham.edu>
2020-12-08 14:47:06 -0800

    able to create a button for starship results from axios call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+//import useState and useEffect from react
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+//import from react-router-dom
+import {Link, Route} from 'react-router-dom';
+
+//axios import 
+import axios from 'axios';
+
 function App() {
+  const [swData, setSwData] = useState([]);
+
+  //useEffect is where the axios call to SW api goes, and the swData state is set to the data it recieves 
+  useEffect(()=> {
+    axios.get('https://swapi.dev/api/starships/')
+    .then((res) => {
+      //to get the starship results you do res.data.results, which is an array of the starships 
+      setSwData(res.data.results);
+    })
+    .catch((err)=> {
+      console.log('AXIOS ERROR', err)
+    })
+  }, [])
+
+  //this function is run everytime the button is clicked for a specific starship
+  const displayShip = (ship) => {
+    console.log('ship object', ship)
+
+  }
+
+  //a function that will render a button for each of the starships with its name displayed 
+  const createButton = () => {
+    console.log('the data', swData)
+    return swData.map((ship) => (
+        <div key={ship.name}>
+          <button onClick= {e => displayShip(ship)}>
+            {ship.name}
+          </button>
+       </div>
+      ))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>STAR WARS STARSHIPS</h1>
+      {/* invoking the createButton function that renders buttons for each starship in the swData state */}
+      {createButton()}
+      
+      
     </div>
   );
 }
